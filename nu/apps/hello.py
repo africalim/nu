6baf782a4c4275fb9179c1567869782cdf5bbe7f
Michael Sarkis <msarkis@protonmail.com>
2024-12-03 16:00:58 +0200

    add clickified hello app

--- nu/apps/hello.py
+++ nu/apps/hello.py
@@ -1,9 +1,17 @@
-import click
+#! /usr/bin/python
 
-@click.command()
-@click.option('--name', prompt='Your name', help='The person to greet.')
-def hello(name):
-    click.echo(f"Hello {name}")
+import click
+from scabha.schema_utils import clickify_parameters
+from omegaconf import OmegaConf
+
+schemas = OmegaConf.load("nu/recipes/hello.yml")
+
+@click.command() 
+@clickify_parameters(schemas.cabs.get("hello"))
+def hello(**kw):
+    # say hello
+    opts = OmegaConf.create(kw)
+    click.echo(f"Hello {opts.name}!")
 
 if __name__ == '__main__':
     hello()
